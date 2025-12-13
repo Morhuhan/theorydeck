18bcf5489d6a49cf857d2ee4c563e89a9c157aa3
morhuhan <radionovich.arkadiy@mail.ru>
2025-12-13 15:10:34 +0500

    Setup Prisma and seed data

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,5 +1,5 @@
-import { PrismaClient } from './generated/client';
 import { PrismaPg } from "@prisma/adapter-pg"
+import { PrismaClient } from "@prisma/client";
 import bcrypt from 'bcryptjs';
 
 const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL });
