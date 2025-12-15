33a9cb6a4248529decbe4feedc14d39cf8479d47
morhuhan <radionovich.arkadiy@mail.ru>
2025-12-15 16:44:26 +0500

    auth fix

--- auth.ts
+++ auth.ts
@@ -2,8 +2,6 @@
 import NextAuth from "next-auth";
 import { authConfig } from "@/lib/auth/auth.config";
 
-const handler = NextAuth(authConfig);
+export const { handlers, auth, signIn, signOut } = NextAuth(authConfig);
 
-export const { GET, POST } = handler;
-
-export const { auth, signIn, signOut } = handler;
+export const { GET, POST } = handlers;
